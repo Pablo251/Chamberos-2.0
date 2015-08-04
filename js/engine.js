af8c7bf8c643102b8902c2c8f727092928e718d2
Pablo Arce Cascante <josepa_arce@hotmail.com>
2015-08-04 14:21:19 -0600

    Changes: JS test in Login
    Test aprpoved!

---
+++ js/engine.js
@@ -0,0 +1,12 @@
+/*Namespace by Pablo Arce Cascante*/
+// function CHAMB = CHAMB || {The target is not confuse with others objects.
+// 	var admin = false;	
+	
+// 	userLogin: function () {
+		
+// 	}
+// }
+function loginUs() {
+	var us = $("#iduser").val();
+	alert(us);
+}
