da3b5b78d6551ebcf0dfb5b187a8ee81d9c64542
Pablo Arce Cascante <josepa_arce@hotmail.com>
2015-08-09 23:48:51 -0600

    Changes index html and engine js

--- js/engine.js
+++ js/engine.js
@@ -10,17 +10,38 @@ var CHAMB = CHAMB || { /*The target is not confuse with others objects.*/
 		if (($("#iduser").val()=="admin")&&($("#idpass").val()=="$uper4dmin")) {
 			this.admin = true;
 			this.actualUser = "Admin";
-		} else {
-			window.location.href = "/Chamberos-2.0/main/chamberos.html";/*Funciona*/
-		}
+		}					
 		var us = $("#iduser").val();
 		alert(us);
+
+		for (var i = 0; i < model.userArray.length; i++) {
+			if ((model.userArray[i].user==$("#iduser").val()) && (model.userArray[i]==$("#idpass").val())) {
+				this.actualUser = $("#iduser").val();
+				this.admin = false;
+				goto link;
+			};
+		};
+		link: 
+		window.location.href = "/Chamberos-2.0/main/chamberos.html";/*Funciona*/
+
+
 	},
 	userLogout: function(){
 		this.admin = false;
 		this.actualUser = '';
 	},
 	model: function(){
+		var userArray = [],/*This array save temp*/
+		chambaArray = [],
+		clientArray = [],
+		invoiceArray = [];		
+		this.loadUserData = function(pUser, pPass){/*This load the users form localStorage*/
 
+		};
+		this.saveUserData = function(pUser, pPass){/*This save an users in the localStorage*/
+			var userObj = {user: pUser, password: pPass};
+			userArray.push(userObj);
+			localStorage.userStorage = JSON.stringify(userArray);
+		};
 	},
 };
