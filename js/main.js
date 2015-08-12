1b2728072164b69d0aa9ccf18ec53a031a891fc9
Pablo Arce Cascante <josepa_arce@hotmail.com>
2015-08-12 03:09:46 -0600

    Changes main js
    id added in the button of admin panel... and to that button added click
    event by jQuery

--- js/main.js
+++ js/main.js
@@ -2,4 +2,7 @@ $(document).ready(function() {
 	// First in execute!
 	debugger;
 	CHAMB.startUp();
+	$("#btnmanage").click(function () {
+		window.location = "/Chamberos-2.0/main/users/";
+	});
 });
