d0702d6c12365cdedd42c7d0ac75a794ef8176ae
Pablo Arce Cascante <josepa_arce@hotmail.com>
2015-08-12 01:51:32 -0600

    Important Changes engine js
    Improvement is on develop... Benefit changes

--- js/engine.js
+++ js/engine.js
@@ -1,52 +1,49 @@
 /*Namespace by Pablo Arce Cascante*/
+var globalUser = "";
 var CHAMB = CHAMB || { /*The target is not confuse with others objects.*/
 	admin: false,
 	actualUser: '',	
 	userLogin: function () {
-		/*$(this).load("/Chamberos-2.0/main/chamberos.html");*//*Open other html incide a stuff*/	
 		/*Validate existing user*/
-		debugger;
-		var mod = new CHAMB.model();
+		var mod = new CHAMB.model();		
 		for (var i = 0; i < mod.loadUserData.length; i++) {
 			if ((mod.loadUserData[i].user==$("#iduser").val()) && (mod.loadUserData[i]==$("#idpass").val())) {
-				this.actualUser = $("#iduser").val();
-				this.admin = false;
-				window.location.href = "/Chamberos-2.0/main/chamberos.html";
+				mod.saveCU(mod.loadUserData[i].user, false);
+				document.location.href = "/Chamberos-2.0/main/chamberos.html";
 				return;
 			};
 		};
 		/*Open the gate for the admin*/
-
-		if (($("#iduser").val()=="admin")&&($("#idpass").val()=="$uper4dmin")) {
-			this.admin = true;
-			this.actualUser = "Admin";
-			window.location.href = "/Chamberos-2.0/main/chamberos.html";/*Funciona*/	
+		if (($("#iduser").val()=="admin")&&($("#idpass").val()=="a")) {
+			mod.saveCU("Admin", true);
+			window.location = "/Chamberos-2.0/main/chamberos.html";
 			return;		
 		}
-		// debugger;
-		/*Error validating here*/
-		/*Get id of label and add a class '.show'*/
-		$("#iderror").addClass("show");
-		/*This put the class '.has-error' in the inputs fields*/
-		$("#field1").addClass("has-error");
+		/*Error validating here*/		
+		$("#iderror").addClass("show");/*Get id of label and add a class '.show'*/		
+		$("#field1").addClass("has-error");/*This put the class '.has-error' in the inputs fields*/
 		$("#field2").addClass("has-error");
 	},
 	userLogout: function(){
 		this.admin = false;
 		this.actualUser = '';
 	},
+	startUp: function(){
+		$("#userName").text(globalUser);
+	},
 	model: function(){
 		this.userArray = [];/*This array save temp*/
 		this.chambaArray = [];
 		this.clientArray = [];
-		this.invoiceArray = [];		
+		this.invoiceArray = [];	
+		this.currentUser = [];
 		this.loadUserData = function(){/*This load the users form localStorage*/
 			if ("userStorage" in localStorage){
 				this.userArray = JSON.parse(localStorage.userStorage);
 			}
 			else{/*Create a new JSON and save this in the local storage*/
 				this.userArray.push({});
-				localStorage.userStorage = JSON.stringify(userArray);
+				localStorage.userStorage = JSON.stringify(this.userArray);
 			}
 			return this.userArray;
 		};
@@ -55,5 +52,17 @@ var CHAMB = CHAMB || { /*The target is not confuse with others objects.*/
 			userArray.push(userObj);
 			localStorage.userStorage = JSON.stringify(userArray);
 		};
+		this.saveCU = function(pUser, pState){
+			debugger;
+			if (!localStorage.currentUser) {/*if currentUser doesn't exist it's created*/
+				localStorage.setItem("currentUser", "");
+			}
+			localStorage.currentUser = "";/*Clear the previous data stored*/
+			this.currentUser.push({user: pUser, state: pState});
+			localStorage.currentUser = JSON.stringify(this.currentUser);	
+		};
+		this.loadCU = function(){
+			return this.currentUser = JSON.parse(localStorage.currentUser);/*Parse the JSON and return a result*/
+		}
 	},
 };
