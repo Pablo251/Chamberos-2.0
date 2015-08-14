05a7844a78d6368c5e696e1108c813a297530c94
Pablo Arce Cascante <josepa_arce@hotmail.com>
2015-08-14 14:14:09 -0600

    Changes to 14 - 08
    Template (Show select or show table), engine (load info in the tables),
    useradmin (launch), user index (improvement), new pis...(us.pg and
    usl.png (more little than us.png))

--- js/engine.js
+++ js/engine.js
@@ -36,6 +36,38 @@ var CHAMB = CHAMB || { /*The target is not confuse with others objects.*/
 			$("#adminpanel").addClass("show");
 		};
 	},
+	userManager: function() {/*Load setings in the user dash... validate a Admin*/
+		var mod = new CHAMB.model();
+		$("#userName").text(mod.loadCU()[0].user);/*Put the current user name*/
+		if (mod.loadCU()[0].state) {
+			$("#usernav").addClass("show");
+			$("div[name = adminDiv]").addClass("show");
+		};
+	},
+	fillUserInfo: function  () {
+		var mod = new CHAMB.model();
+		for (var i = 0; i < mod.loadUserData.length; i++) {
+			CHAMB.loadTables(mod.loadUserData[i].userId, mod.loadUserData[i].name, mod.loadUserData[i].lastName, mod.loadUserData[i].userName, null,4);
+		};		
+	},
+	loadTables: function (pId, p1, p2, p3, p4, p5, pCellNums) {/*Load info on tables and in the select*/
+		var tableRow = document.getElementById("idtable").insertRow();/*Table Object, insert a new row*/
+		var selectOtion = $("#selectmov");/*Select Object*/
+		var cell0 = tableRow.insertCell(0),
+		cell1 = tableRow.insertCell(1),
+		cell2 = tableRow.insertCell(2),
+		cell3 = tableRow.insertCell(3),
+		cell4 = tableRow.insertCell(4),
+		cellOptions = tableRow.insertCell(5);
+		/*If is a table with only 4 cells...*/
+		cell0.innerHTML = p1;
+		cell1.innerHTML = p2;
+		cell2.innerHTML = p3;
+		cell3.innerHTML = p4;
+		if (pCellNums>5)
+			cell4.innerHTML = p5;
+		/*add buttons for edit and delete*/
+	},
 	model: function(){
 		this.userArray = [];/*This array save temp*/
 		this.chambaArray = [];
@@ -43,14 +75,7 @@ var CHAMB = CHAMB || { /*The target is not confuse with others objects.*/
 		this.invoiceArray = [];	
 		this.currentUser = [];
 		this.loadUserData = function(){/*This load the users form localStorage*/
-			if ("userStorage" in localStorage){
-				this.userArray = JSON.parse(localStorage.userStorage);
-			}
-			else{/*Create a new JSON and save this in the local storage*/
-				this.userArray.push({});
-				localStorage.userStorage = JSON.stringify(this.userArray);
-			}
-			return this.userArray;
+			return this.userArray = JSON.parse(localStorage.userStorage);
 		};
 		this.saveUserData = function(pUser, pPass){/*This save an users in the localStorage*/
 			var userObj = {user: pUser, password: pPass};
@@ -61,13 +86,13 @@ var CHAMB = CHAMB || { /*The target is not confuse with others objects.*/
 			debugger;
 			if (!localStorage.currentUser) {/*if currentUser doesn't exist it's created*/
 				localStorage.setItem("currentUser", "");
-		}
-		localStorage.currentUser = "";/*Clear the previous data stored*/
-		this.currentUser.push({user: pUser, state: pState});
-		localStorage.currentUser = JSON.stringify(this.currentUser);	
+			localStorage.currentUser = "";/*Clear the previous data stored*/
+			this.currentUser.push({user: pUser, state: pState});
+			localStorage.currentUser = JSON.stringify(this.currentUser);
+		};
+		this.loadCU = function(){/*load a current user information*/
+			return this.currentUser = JSON.parse(localStorage.currentUser);/*Parse the JSON and return a result*/
+		};
 	};
-	this.loadCU = function(){
-		return this.currentUser = JSON.parse(localStorage.currentUser);/*Parse the JSON and return a result*/
-	}
 },
 };
---
+++ js/useradmin.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {	
+	/*CHAMB.userManager();*/
+	/*CHAMB.fillInfo();*/
+/*	$("#btnmanage").click(function () {
+		window.location = "/Chamberos-2.0/main/users/new-user.html";
+	});
+	$("#btnmovdelete").click(function () {
+		window.location = "/Chamberos-2.0/main/users/delete-user.html";
+	});
+	$("#btnmovedit").click(function () {
+		window.location = "/Chamberos-2.0/main/users/edit-user.html";
+	});	
+	$("#logOut").click(CHAMB.userLogtou);*/ 
+});
