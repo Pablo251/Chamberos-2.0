8f80b607158bfe828a3ef0a4225850a6f30e8222
Pablo Arce Cascante <josepa_arce@hotmail.com>
2015-08-15 14:34:21 -0600

    Changes newuser js
    Functions and events added

---
+++ js/newuser.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {	
+	CHAMB.startUp();
+	$("#btnmanage").click(function () {
+		window.location = "/Chamberos-2.0/main/users/new-user.html";
+	});
+	$("#logOut").click(CHAMB.userLogout);
+	$("#cancel").click(function () { /*Cancel button*/
+		window.location = "/Chamberos-2.0/main/users/";
+	});	
+	$("#save").click(CHAMB.saveUser);
+});
