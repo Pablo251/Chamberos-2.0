3732429d68c14c211363d2ff119ab402c0effd0b
Pablo Arce Cascante <josepa_arce@hotmail.com>
2015-08-17 18:37:19 -0600

    Changes edituser js
    Model instance added and "cancel" and "save" events improvement

--- js/edituser.js
+++ js/edituser.js
@@ -2,7 +2,11 @@ $(document).ready(function () {
 	debugger;
 	CHAMB.startUp();
 	CHAMB.editUserLoad();
-	$("#cancel").click(function () {
+	var mod = new CHAMB.model();
+	$("#cancel").click(function () {		
+		mod.globalIdSet(0);
 		window.location = "/Chamberos-2.0/main/users/";
 	});
+	CHAMB.actualId = $("#iduser").val();
+	$("#save").click(CHAMB.editUser);
 });
