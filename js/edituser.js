19e5571dc7249ca285737c63927273a27af88773
Pablo Arce Cascante <josepa_arce@hotmail.com>
2015-08-17 23:50:47 -0600

    CHanges edituser js
    FIxed

--- js/edituser.js
+++ js/edituser.js
@@ -1,5 +1,4 @@
-$(document).ready(function () {
-	debugger;
+$(document).ready(function () {	
 	CHAMB.startUp();
 	CHAMB.editUserLoad();
 	var mod = new CHAMB.model();
@@ -7,6 +6,7 @@ $(document).ready(function () {
 		mod.globalIdSet(0);
 		window.location = "/Chamberos-2.0/main/users/";
 	});
+	debugger;
 	CHAMB.actualId = $("#iduser").val();
 	$("#save").click(CHAMB.editUser);
 });
